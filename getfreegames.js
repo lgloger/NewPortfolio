f80915fa0b6ada0ebceb054f4a4ae405f0e7cd05
Luca <luca.kloger@gmail.com>
2025-08-30 21:07:53 +0200

    Test

--- getfreegames.js
+++ getfreegames.js
@@ -1,4 +1,5 @@
 const api = "https://www.cheapshark.com/api/1.0/deals?storelD=25&upperPrice=0";
+import { CLIENT_ID, TOKEN } from "./igdbApi.js";
 
 async function getfreeGames() {
   try {
@@ -12,18 +13,22 @@ async function getfreeGames() {
   }
 }
 
-displayFreeEpicGames = (data) => {
+async function displayFreeEpicGames(data) {
   const freeGamesList = document.querySelector(".freeGamesList");
 
   for (let i = 0; i < data.length; i++) {
     const game = data[i];
-    gameThumb = game.thumb;
-    gameTitle = game.title;
-    normalPrice = game.normalPrice;
-    rating = game.steamRatingText;
+    let dealID = game.dealID;
+    let gameTitle = game.title;
+    let gameThumb = game.thumb;
+    let normalPrice = game.normalPrice;
+    let rating = game.steamRatingText;
 
-    const gameCard = document.createElement("div");
+    let gameCover = await getGameIcon(gameTitle);
+
+    const gameCard = document.createElement("a");
     gameCard.classList.add("gameCard");
+    gameCard.href = `https://www.cheapshark.com/redirect?dealID=${dealID}`;
     gameCard.innerHTML = `
             <img src="${gameThumb}" alt="${gameTitle}" class="gameThumb"/>
             <div class="gameInfo">
@@ -37,6 +42,44 @@ displayFreeEpicGames = (data) => {
 
     freeGamesList.appendChild(gameCard);
   }
-};
+}
+
+async function getGameIcon(gameName) {
+  async function getGameId(name) {
+    const res = await fetch("https://api.igdb.com/v4/games", {
+      method: "POST",
+      headers: {
+        "Client-ID": CLIENT_ID,
+        Authorization: `Bearer ${TOKEN}`,
+        Accept: "application/json",
+      },
+      body: `fields id,name; search "${name}"; limit 1;`,
+    });
+    const data = await res.json();
+    return data[0]?.id;
+  }
+
+  async function getGameCover(gameId) {
+    const res = await fetch("https://api.igdb.com/v4/covers", {
+      method: "POST",
+      headers: {
+        "Client-ID": CLIENT_ID,
+        Authorization: `Bearer ${TOKEN}`,
+        Accept: "application/json",
+      },
+      body: `fields url; where game = ${gameId};`,
+    });
+    const data = await res.json();
+    return data[0]?.url ? `https:${data[0].url}` : null;
+  }
+
+  (async () => {
+    const gameId = await getGameId(gameName);
+    if (!gameId) return console.log("Spiel nicht gefunden");
+
+    const coverUrl = await getGameCover(gameId);
+    console.log(coverUrl);
+  })();
+}
 
 getfreeGames();
