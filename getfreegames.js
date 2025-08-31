7971b6723d8b006ac4860e491bbf40a467257f13
Luca <luca.kloger@gmail.com>
2025-08-31 18:56:20 +0200

    Added Genres

--- getfreegames.js
+++ getfreegames.js
@@ -1,3 +1,5 @@
+import { tagsToGenre } from "./epicTagsID.js";
+
 const api =
   "https://corsproxy.io/https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=de-DE&country=DE&allowCountries=DE";
 
@@ -30,9 +32,23 @@ async function displayFreeEpicGames(data) {
     let status = "";
     let statusClass = "";
 
+    let upcomingDate = "";
+
     if (game.promotions.upcomingPromotionalOffers.length > 0) {
       status = "UPCOMING";
       statusClass = "upcoming";
+      let date =
+        game.promotions.upcomingPromotionalOffers[0].promotionalOffers[0]
+          .startDate;
+      let dateObj = new Date(date);
+
+      upcomingDate = dateObj.toLocaleString("de-DE", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
     } else {
       status = "FREE NOW";
     }
@@ -45,6 +61,10 @@ async function displayFreeEpicGames(data) {
     let gameTitle = game.title;
     let normalPrice = game.price.totalPrice.fmtPrice.originalPrice || "0,00 €";
 
+    let tags = game.tags;
+    let genres = tags.map((tag) => tagsToGenre.get(tag.id)).filter(Boolean);
+    let genresText = genres.join(", ") || "–";
+
     const gameCard = document.createElement("a");
     gameCard.classList.add("gameCard");
     gameCard.href = `https://www.epicgames.com/store/de/p/${pageSlug}`;
@@ -53,9 +73,11 @@ async function displayFreeEpicGames(data) {
             <div class="statusBadge ${statusClass}">${status}</div>
             <img src="${gameCover}" alt="${gameTitle}" class="gameThumb"/>
             <div class="gameInfo">
-            <span class="gameTitle">${gameTitle}</span>
+              <span class="gameTitle">${gameTitle}</span>
+              <span class="gameGenres">${genresText}</span>
             <div class="InfoText">
-            <span class="normalGamePrice">${normalPrice}</span>
+              <span class="normalGamePrice">${normalPrice}</span>
+              <span class="gameDate">${upcomingDate}</span>
             </div>
             </div>
             `;
